b74c6e81fcb133357a31f427bf8a58d0c50b86b5
Rahul Garg <rahulgarg@Rahuls-MacBook.local>
2014-07-02 19:54:51 +0530

    Initial Commit

---
+++ BasicUIView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BasicUIView
+//
+//  Created by Rahul Garg on 02/07/14.
+//  Copyright (c) 2014 Rahul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
